27bcfd8bb2beb6c38d48220f8440937d105d6b0c
Yaash Jain <yaash.jain45@gmail.com>
2025-10-18 21:08:35 +0530

    Add global logging module and replace prints

---
+++ src/text_classifier/logger.py
@@ -0,0 +1,40 @@
+import logging
+
+from rich.logging import RichHandler
+
+
+def configure_logger(verbosity: int):
+    """
+    Configure the root logger of the application dynamically.
+
+    The root logger will be configured to log at either the
+    DEBUG or INFO level, depending on the verbosity parameter.
+    """
+    if verbosity >= 1:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    # Update the root logger’s level dynamically
+    logging.getLogger().setLevel(level)
+
+    # Configure logging once, globally
+    logging.basicConfig(
+        level=level,
+        format="[%(name)s] %(message)s",
+        datefmt="%H:%M:%S",
+        handlers=[RichHandler(rich_tracebacks=True, markup=True)],
+    )
+
+
+def get_logger(name: str) -> logging.Logger:
+    """
+    Return a logger with the given name.
+
+    Args:
+        name: The name of the logger to return.
+
+    Returns:
+        A logger with the given name.
+    """
+    return logging.getLogger(name)
--- src/text_classifier/nlp.py
+++ src/text_classifier/nlp.py
@@ -6,6 +6,18 @@ It is contained within this separate module to serve as a
 pythonic 'singleton', as it will only be loaded once.
 """
 
+from time import perf_counter
+
 import spacy
 
+from .logger import get_logger
+
+logger = get_logger(__name__)
+
+import_start = perf_counter()
+
 nlp_model = spacy.load("en_core_web_sm")
+
+import_end = perf_counter()
+
+logger.debug(f"nlp model loading complete. time elapsed: {import_end - import_start}s")
