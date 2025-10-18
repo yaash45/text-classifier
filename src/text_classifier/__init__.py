c068254096f0a585abab66512e2ff84f5abd123b
Yaash Jain <yaash.jain45@gmail.com>
2025-10-18 21:10:05 +0530

    Add global logging module and replace prints (#3)

--- src/text_classifier/__init__.py
+++ src/text_classifier/__init__.py
@@ -1,9 +1,41 @@
+import argparse
+
 from .features import Vocabulary, WordBag, vectorize
+from .logger import configure_logger, get_logger
 from .parser import parse_words
 
 
+def parse_args():
+    """
+    Parse command line arguments into an argparse.Namespace object.
+
+    Current arguments parsed: `verbose`
+
+    Returns:
+        argparse.Namespace: containing the parsed command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Run the text-classifier app.")
+
+    # The magic: `-v` can be repeated to increase verbosity
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase verbosity (use -v, -vv, -vvv for more detail)",
+    )
+
+    return parser.parse_args()
+
+
 def main() -> None:
-    print("Hello from text-classifier!\n")
+    args = parse_args()
+
+    configure_logger(args.verbose)
+
+    logger = get_logger(__name__)
+
+    logger.info("Hello from text-classifier!\n")
 
     docs: list[str] = [
         "this is Tony Soprano, the mafia don of New Jersey. Gabagool is his favourite food. Bring him some Gabagool from Satrialles in New Jersey",
@@ -23,9 +55,9 @@ def main() -> None:
         bag = WordBag(words)
         bags.append(bag)
 
-    print(f"vocab = {vocab}\n")
+    logger.debug(f"vocab = {vocab}\n")
 
     for i, bag in enumerate(bags):
-        print(f"bag [{i}] = {bag}")
+        logger.info(f"bag [{i}] = {bag}")
         vector = vectorize(bag, vocab)
-        print(f"         - {vector}\n")
+        logger.debug(f"{vector}\n")
